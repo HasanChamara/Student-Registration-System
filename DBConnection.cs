4f90951137e9b1509a6a75904bcb54ab03213a95
HasanChamara <hasanchamara1234@gmail.com>
2024-09-17 21:53:34 +0530

    DB Connection

---
+++ DBConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistrationSystem
+{
+    internal class DBConnection
+    {
+        public string MyConnection()
+        {
+           string conn = @"Data Source=DESKTOP-REGH4GI\SQLEXPRESS;Initial Catalog=Student;Integrated Security=True;";
+            return conn;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -7,15 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace StudentRegistrationSystem
 {
     public partial class Form1 : Form
     {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnection dbcon = new DBConnection();
         public Form1()
         {
             InitializeComponent();
-            // This is the constructor of the form
+            cn = new SqlConnection(dbcon.MyConnection());
+            cn.Open();
+            MessageBox.Show("Connection Successful");
         }
     }
 }
